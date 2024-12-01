6159771e90d5c58a8d58cec2f23cf85fc9bb699e
Andraz Sovinec <andraz.sovinec@gmail.com>
2024-12-01 11:29:48 +0100

    Added functionality for registering user

--- UserSrevice/models/User.js
+++ UserSrevice/models/User.js
@@ -1,2 +1,36 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  });
+
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function(next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err)
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
