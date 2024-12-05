7fbbae8778a7618825238dfe024b23d05253d8ab
Andraz Sovinec <andraz.sovinec@gmail.com>
2024-12-05 17:03:00 +0100

    Added login functionality

--- UserSrevice/controllers/userController.js
+++ UserSrevice/controllers/userController.js
@@ -18,3 +18,25 @@ exports.registerUser = async (request, result) => {
         result.status(500).json({ message: 'Server error:', error: err });
     }
 }; 
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'This user does not exist' });
+    }
+
+    const matchingPass = await user.comparePassword(password);
+    if (!matchingPass) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    const jwtoken = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Login successful', jwtoken });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+};
